3cd4f354bd0b4e9dd482700a292238261ebfbab5
Ania Kiedrowska <kiedrowska.ania@gmail.com>
2024-01-05 13:48:56 +0100

    push to incomes

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let incomeName = document.querySelector("#income-name");
-  let incomeAmount = document.querySelector("#income-amount");
-  let addIncome = document.querySelector("#add-income");
-});
+let incomeName = document.querySelector("#income-name");
+let incomeAmount = document.querySelector("#income-amount");
+let addIncome = document.querySelector("#add-income");
+let incomeList = document.querySelector("#income-list");
 
 let incomes = [];
+
 addIncome.addEventListener("click", () => {
   incomes.push({ title: incomeName.value, amount: incomeAmount.value });
+
+  let incomeElement = `<li class="list-element">
+  <span>${incomeName.value}</span><span>${incomeAmount.value}</span>
+</li>`;
+
+  incomeList.appendChild(incomeElement);
 });
