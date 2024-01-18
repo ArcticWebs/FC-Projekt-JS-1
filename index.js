99ddaef7c94dc548af042f95aebe4f60d3e798e6
Ania Kiedrowska <kiedrowska.ania@gmail.com>
2024-01-18 15:13:24 +0100

    adding list changed

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ const updateTotalBalance = () => {
 
   totalInOut.innerText = totalText();
 };
+
 updateTotalBalance();
 
 // ADDING INCOMES
@@ -107,19 +108,39 @@ const updateOutcomeList = () => {
     const outcomeItem = document.createElement("li");
     outcomeItem.classList.add("list-element");
 
-    const outcomeElement = `
-<span>${outcome.title}</span><span>${outcome.amount}</span>
-<div class="list-icons">
-<img
-src="assets/PencilSimple.png"
-alt=""
-class="edit-delete-icon"
-id="edit"
-/>
-<img src="assets/Trash.png" alt="" class="edit-delete-icon" />
-</div>`;
+    const outcomeNameText = document.createElement("span");
+    outcomeNameText.innerText = `${outcome.title}`;
 
-    outcomeItem.innerHTML = outcomeElement;
+    const outcomeValueNumber = document.createElement("span");
+    outcomeValueNumber.innerText = ` ${outcome.amount}`;
+
+    const outcomeIconsBox = document.createElement("div");
+    outcomeIconsBox.classList.add("list-icons");
+
+    const editIcon = document.createElement("img");
+    editIcon.src = "assets/PencilSimple.png";
+    editIcon.setAttribute("alt", "");
+    editIcon.classList.add("edit-delete-icon");
+
+    const deleteIcon = document.createElement("img");
+    deleteIcon.src = "assets/Trash.png";
+    deleteIcon.setAttribute("alt", "");
+    deleteIcon.classList.add("edit-delete-icon");
+    deleteIcon.addEventListener("click", () => {
+      outcome.splice(0, 1);
+
+      updateOutcomeList();
+      updateTotalOutcome();
+      updateTotalBalance();
+    });
+
+    outcomeIconsBox.appendChild(editIcon);
+    outcomeIconsBox.appendChild(deleteIcon);
+
+    outcomeItem.appendChild(outcomeNameText);
+    outcomeItem.appendChild(outcomeValueNumber);
+    outcomeItem.appendChild(outcomeIconsBox);
+
     outcomeList.appendChild(outcomeItem);
   });
 };
