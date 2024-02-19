1c401f8488e4242d97061957f30bf7bba504f7e5
Ania Kiedrowska <kiedrowska.ania@gmail.com>
2024-02-19 11:49:20 +0100

    outcomes editing function updated

--- scripts/outcomes.js
+++ scripts/outcomes.js
@@ -4,7 +4,7 @@ const outcomesTotal = document.querySelector("#outcomes-total");
 
 import { updateTotalBalance } from "./index.js";
 
-export const outcomes = [];
+export let outcomes = [];
 
 export const getOutcomesSum = () => {
   return outcomes.reduce((acc, element) => {
@@ -26,9 +26,11 @@ const updateOutcomeList = () => {
 
     const outcomeNameText = document.createElement("span");
     outcomeNameText.innerText = `${outcome.title}`;
+    outcomeNameText.id = "outcome-name";
 
     const outcomeValueNumber = document.createElement("span");
     outcomeValueNumber.innerText = ` ${outcome.amount}`;
+    outcomeValueNumber.id = "outcome-value";
 
     const outcomeIconsBox = document.createElement("div");
     outcomeIconsBox.classList.add("list-icons");
@@ -37,6 +39,90 @@ const updateOutcomeList = () => {
     editIcon.src = "assets/PencilSimple.png";
     editIcon.setAttribute("alt", "edit icon");
     editIcon.classList.add("edit-delete-icon");
+    editIcon.addEventListener("click", () => {
+      let listElementoToEdit = editIcon.closest("li");
+      let outcomeNameToEdit = listElementoToEdit.querySelector("#outcome-name");
+      let outcomeValueToEdit =
+        listElementoToEdit.querySelector("#outcome-value");
+      let outcomeListIcons = listElementoToEdit.querySelector(".list-icons");
+
+      let actualOutcomeName = outcomeNameToEdit.innerText;
+      let actualOutcomeValue = outcomeValueToEdit.innerText;
+
+      outcomeNameToEdit.innerHTML = "";
+
+      const nameEditigInput = document.createElement("input");
+      nameEditigInput.classList.add("main-input");
+      nameEditigInput.setAttribute("type", "text");
+      nameEditigInput.setAttribute("minlength", "3");
+      nameEditigInput.setAttribute("value", actualOutcomeName);
+      outcomeNameToEdit.appendChild(nameEditigInput);
+
+      outcomeValueToEdit.innerHTML = "";
+
+      const valueEditigInput = document.createElement("input");
+      valueEditigInput.classList.add("main-input", "number-input");
+      valueEditigInput.setAttribute("type", "number");
+      valueEditigInput.setAttribute("value", actualOutcomeValue);
+      outcomeNameToEdit.appendChild(valueEditigInput);
+
+      outcomeListIcons.innerHTML = "";
+
+      const doneIcon = document.createElement("img");
+      doneIcon.src = "assets/done.png";
+      doneIcon.setAttribute("alt", "");
+      doneIcon.classList.add("edit-delete-icon");
+      outcomeListIcons.appendChild(doneIcon);
+      doneIcon.addEventListener("click", () => {
+        const _title = nameEditigInput.value;
+        const _value = valueEditigInput.value;
+
+        outcomes = outcomes.map((el) => {
+          if (el.id === outcome.id) {
+            return {
+              ...el,
+              title: _title,
+              amount: Number(_value),
+            };
+          }
+          return el;
+        });
+
+        // updateOutcomeList();
+        updateTotalOutcome();
+        updateTotalBalance();
+
+        outcomeNameToEdit.innerHTML = "";
+
+        const outcomeNameText = document.createElement("span");
+        outcomeNameText.innerText = `${_title}`;
+        outcomeNameText.id = "outcome-name";
+        outcomeNameToEdit.appendChild(outcomeNameText);
+
+        outcomeValueToEdit.innerHTML = "";
+
+        const outcomeValueNumber = document.createElement("span");
+        outcomeValueNumber.innerText = ` ${Number(_value)}`;
+        outcomeValueNumber.id = "outcome-value";
+        outcomeValueToEdit.appendChild(outcomeValueNumber);
+
+        outcomeIconsBox.innerHTML = "";
+
+        outcomeIconsBox.appendChild(editIcon);
+        outcomeIconsBox.appendChild(deleteIcon);
+      });
+
+      const cancelIcon = document.createElement("img");
+      cancelIcon.src = "assets/cancel.png";
+      cancelIcon.setAttribute("alt", "");
+      cancelIcon.classList.add("edit-delete-icon");
+      outcomeListIcons.appendChild(cancelIcon);
+      cancelIcon.addEventListener("click", () => {
+        // updateOutcomeList();
+        updateTotalOutcome();
+        updateTotalBalance();
+      });
+    });
 
     const deleteIcon = document.createElement("img");
     deleteIcon.src = "assets/Trash.png";
